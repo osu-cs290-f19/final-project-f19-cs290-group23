98d83b44e47f8bb7c0cdd4716b7acbc23433682f
Keith Stevens <mrkeith782@gmail.com>
2019-12-10 02:11:45 -0800

    added extra url tests

--- server.js
+++ server.js
@@ -10,11 +10,31 @@ app.set('view engine', 'handlebars');
 app.use(express.static('public'));
 
 var posts = require('./posts.json');
+var views = fs.readdirSync('./views');
+
+for(var i = 0; i < views.length; i++) {
+    if (views[i].includes("handlebars")) {
+        views[i] = views[i].substring(0, views[i].length-11);
+    };
+}
+
+console.log(views);
 
 app.get('/', function (req, res, next) {
     res.render('posts', {posts : posts});
     res.status(200);
 });
+
+app.get('/:url', function (req, res, next) {
+    for(var i = 0; i < views.length; i++) {
+        if (views[i] == req.params.url) {
+            res.render(req.params.url);
+            res.status(200);
+            return;
+        }
+    }
+    next();
+})
 
 app.get('*', function (req, res) {
     res.render('partials/404');
