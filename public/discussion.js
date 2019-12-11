260a0cc0315b102a7e62f1a6d10ad7b9b2bef301
Keith Stevens <mrkeith782@gmail.com>
2019-12-11 12:13:55 -0800

    finished stuff

--- public/discussion.js
+++ public/discussion.js
@@ -12,22 +12,24 @@ for (var i = 0; i < likebuttons.length; i++) {
     likebuttons[i].addEventListener('click', like.bind(likebuttons[i], i));
 }
 
+function aaa(index, event) {
+    var temp = document.getElementsByClassName('addcomment');
+    console.log(temp[index]);
+    var newcomments = {
+        commentary : temp[index].value
+    };
+    var request = new XMLHttpRequest();
+    var requestBody = JSON.stringify(newcomments);
+    request.open('POST', '/changecommentary/');
+    request.setRequestHeader(
+        'Content-Type', 'application/json'
+    );
+    request.send(requestBody);
+    window.location.href = '/';
+}
+
 var submit = document.getElementsByClassName('submitinfo');
 
 for (var i = 0; i < submit.length; i++) {
-    submit[i].addEventListener('click', function() {
-        var temp = document.getElementsByClassName('addcomment');
-        console.log(temp[parseInt(i)]);
-        var newcomments = {
-            commentary : temp[i].nodeValue
-        };
-        var request = new XMLHttpRequest();
-        var requestBody = JSON.stringify(newcomments);
-        request.open('POST', '/changecommentary/');
-        request.setRequestHeader(
-            'Content-Type', 'application/json'
-        );
-        request.send(requestBody);
-        window.location.href = '/';
-    });
+    submit[i].addEventListener('click', aaa.bind(i,i));
 }
