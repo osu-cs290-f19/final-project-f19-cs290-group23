ae6f29ba3727173a88f6a0ba048a0d86f9fdbdcd
unknown <mrkeith782@gmail.com>
2019-12-10 21:15:47 -0800

    did work to allow client-side posts

--- public/index.js
+++ public/index.js
@@ -1,6 +1,18 @@
-var popularposts = document.getElementById('pop-posts-button');
-function dropdownmenu() {
+var newname = document.getElementById('newname');
+var newusername = document.getElementById('newusername');
+var newphotourl = document.getElementById('newphotourl');
+var newingredients = document.getElementById('newingredients');
+var newrecipemade = document.getElementById('newrecipe');
+var newnotes = document.getElementById('newnotes');
+
+function makeNewPost() {
     
 }
 
-popularposts.addEventListener(onclick, showmenu());
+document.getElementById("refreshbutton").addEventListener("click", function(){
+    Handlebars.templates.blankpost(newphotourl.value, newname.value, newusername.value, newingredients.value, newrecipemade.value, newnotes.value);
+});
+
+document.getElementById("submitbutton").addEventListener('click', function() {
+    //todo
+});
