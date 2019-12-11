63c0811e6ba4c8690d3ae87605ef9f86b166807a
Keith Stevens <mrkeith782@gmail.com>
2019-12-11 01:03:24 -0800

    added dynamic client post creation

--- public/index.js
+++ public/index.js
@@ -5,9 +5,6 @@ var newingredients = document.getElementById('newingredients');
 var newrecipemade = document.getElementById('newrecipe');
 var newnotes = document.getElementById('newnotes');
 
-var request = new XMLHttpRequest();
-var requestURL = '/addrecipe';
-
 function makeNewPost() {
     
 }
@@ -21,7 +18,7 @@ document.getElementById("refreshbutton").addEventListener("click", function(){
         ingredients: newingredients.value,
         recipe: newrecipemade.value,
         commentary: newnotes.value
-    }
+    };
 
     if (newphotourl.value == '' || newphotourl.value == undefined) {
         post.imgurl = "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwiS84iS8qzmAhWYsZ4KHfPvD5MQjRx6BAgBEAQ&url=https%3A%2F%2Fcatchoom.com%2Fblog%2F&psig=AOvVaw051r5SUPFdTUkIVozLv9z9&ust=1576129516486120";
@@ -36,6 +33,30 @@ document.getElementById("refreshbutton").addEventListener("click", function(){
     content[0].insertAdjacentHTML("beforeend", refresh);
 });
 
+var request = new XMLHttpRequest();
+var requestURL = '/addrecipe';
 document.getElementById("submitbutton").addEventListener('click', function() {
-    //todo
+    if(newname.value && newusername.value && newphotourl.value && newingredients.value && newrecipemade.value && newnotes.value) {
+        post = {
+            title: newname.value,
+            user: newusername.value,
+            imgurl: newphotourl.value,
+            ingredients: newingredients.value,
+            recipe: newrecipemade.value,
+            commentary: newnotes.value
+        };
+        var requestBody = JSON.stringify(post);
+        request.open('POST', requestURL);
+        request.setRequestHeader(
+            'Content-Type', 'application/json'
+        );
+        window.location.href = '/';
+    }
+    else {
+        alert("Not all boxes are filled!"); //todo do more?
+    }
+});
+
+request.addEventListener('load', function() {
+    console.log('Success!');
 });
